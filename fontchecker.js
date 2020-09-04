54f740e1536c027bbfa0b8126e0b672142e89216
Gabriel <54387571+bieldlucacn@users.noreply.github.com>
2020-09-04 00:17:29 -0300

    Atualizei o repo

--- fontchecker.js
+++
@@ -1,7 +0,0 @@
-import { Typefont } from "fontchecker/src/index.js";
-
-async function getFontFromImage (fontchecker/ocr/1.jpeg) {
-    const fonts = await Typefont(fontchecker/ocr/1.jpeg);
-    
-    return fonts[0]; // Return the most similar font.
-}    
